d9bb7fd9d9b320cc441aaf5a014ba1cb17335bb5
Parth Dalvi <124716803+ParthDalvi164@users.noreply.github.com>
2025-08-22 21:00:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let slider = document.getElementById("mySlider");
-    let output = document.getElementById("demo");
-    output.innerHTML = slider.value;
-
-    slider.oninput = function() {
-      output.innerHTML = this.value;
-    }
